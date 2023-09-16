4c4e5388c230eb41f8cdfc333d6714ef93bb96e1
Alikhan-Pvl <108364082+Alikhan-Pvl@users.noreply.github.com>
2023-09-16 11:42:02 +0600

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,37 @@
 print ("hello world")
+
+num=9
+float_num=9.1
+print(num, float_num,sep='\t')
+a=19
+b=19.00
+print('a= '+str(a))
+print('b= '+str(b))
+
+var_int=42
+var_flt=42.0
+print(var_int,'\n')
+print(var_flt,'\n')
+
+name = input('Enter your name plese: ')
+print('The name is ' + name,'\n')
+
+name = 'C3PO'
+age = 112
+print(f"Salem, My name is {name} and I'm {age} years old.")
+
+
+
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num   
+    print("Result:", result,'\n')
+except:
+    print("Division by zero is not allowed.",'\n')
+
+try:
+    num2 = int(input("Enter a number: "))
+    result2 = 10 / num2
+    print("Result:", result2,'\n')
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.",'\n')
